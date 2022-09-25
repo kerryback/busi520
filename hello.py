0607cc5884c73fae0e87f1ddc332682c89e28369
kerryback <kerryback@gmail.com>
2022-09-25 16:28:11 -0500

    original commit

--- hello.py
+++
@@ -1,2 +0,0 @@
-name = input('Please type your name: ')
-print("Hello " + name)
--- myscript.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-name = sys.argv[1]
-print("Hello", name)
-with open("hello.txt", "w") as f:
-    f.write("Hello " + name)
